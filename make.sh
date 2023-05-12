8af97c3cbc5868eaec33e2b8a5f33b90a33ae4e9
Cao Tengfei <tengfei.cao@anlogic.com>
2023-05-12 20:02:57 +0800

    [][fix][driver][RPU] fix compile toolchain and s-mode cache ops enable [rootcause]:fix cache error [side effect]:fix cache with uboot, pull together
    Change-Id: Ib605c9c99377fcf090ecbc69bf1e9586842afdf2

--- make.sh
+++ make.sh
@@ -1,2 +1,2 @@
-make CROSS_COMPILE=../toolchain/riscv/gcc/bin/riscv-nuclei-linux-gnu- PLATFORM_RISCV_ABI=lp64 PLATFORM_RISCV_ISA=rv64imac PLATFORM=nuclei/demosoc
+make CROSS_COMPILE=../toolchains/riscv/gcc/bin/riscv-nuclei-linux-gnu- PLATFORM_RISCV_ABI=lp64 PLATFORM_RISCV_ISA=rv64imac PLATFORM=nuclei/demosoc
 #cp build/platform/nuclei/demosoc/firmware/fw_jump.bin .
