3b18814f030fd81fd947118a6aca50166618e5c8
fushan.zeng <fushan.zeng@anlogic.com>
2023-08-18 17:24:06 +0800

    enable fpu

--- make.sh
+++ make.sh
@@ -1,2 +1,2 @@
-make CROSS_COMPILE=../toolchains/riscv/gcc/bin/riscv-nuclei-linux-gnu- PLATFORM_RISCV_ABI=lp64 PLATFORM_RISCV_ISA=rv64imac PLATFORM=anlogic/dr1v90
+make CROSS_COMPILE=../toolchains/riscv/gcc/bin/riscv-nuclei-linux-gnu- PLATFORM_RISCV_ABI=lp64 PLATFORM_RISCV_ISA=rv64imafdc PLATFORM=anlogic/dr1v90
 #cp build/platform/anlogic/dr1v90/firmware/fw_jump.bin .
