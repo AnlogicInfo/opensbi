042295419b5d91f0e1d177b33d95b51ecbb82d6b
fushan.zeng <fushan.zeng@anlogic.com>
2023-12-18 09:42:15 +0800

    [platform]: refine config.mk

--- platform/anlogic/dr1v90/config.mk
+++ platform/anlogic/dr1v90/config.mk
@@ -22,14 +22,17 @@ platform-ldflags-y =
 platform-runcmd =
 
 # Blobs to build
-FW_TEXT_START ?= 0x00000000
+ifndef FW_TEXT_START
+override FW_TEXT_START = 0x00000000
+endif
 FW_DYNAMIC=y
 FW_JUMP=y
 # This needs to be 2MB aligned for 64-bit system
-FW_JUMP_ADDR=$(shell printf "0x%X" $$(($(FW_TEXT_START) + 0x200000)))
+ifndef FW_JUMP_ADDR
+override FW_JUMP_ADDR = $(shell printf "0x%X" $$(($(FW_TEXT_START) + 0x200000)))
+endif
 # FW_JUMP_FDT_ADDR=$(shell printf "0x%X" $$(($(FW_TEXT_START) + 0x8000000)))
 FW_PAYLOAD=y
 # This needs to be 2MB aligned for 64-bit system
 FW_PAYLOAD_OFFSET=0x200000
 # FW_PAYLOAD_FDT_ADDR=$(FW_JUMP_FDT_ADDR)
-
