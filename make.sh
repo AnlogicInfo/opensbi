86c89c939206b13de6da7e26bf22801ee9da13a9
fushan.zeng <fushan.zeng@anlogic.com>
2024-04-08 14:04:27 +0800

    test: test merge
    test merge
    
    Signed-off-by: fushan.zeng <fushan.zeng@anlogic.com>

--- make.sh
+++ make.sh
@@ -1,2 +1,2 @@
 make CROSS_COMPILE=../toolchains/riscv/gcc/bin/riscv-nuclei-linux-gnu- PLATFORM_RISCV_ABI=lp64 PLATFORM_RISCV_ISA=rv64imafdc PLATFORM=anlogic/dr1v90
-#cp build/platform/anlogic/dr1v90/firmware/fw_jump.bin .
+#cp build/platform/anlogic/dr1v90/firmware/fw_jump.bin . 
